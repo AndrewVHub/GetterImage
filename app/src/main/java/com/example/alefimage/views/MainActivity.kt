3f39653135f544868d2b3dc4dfb0a33448894b82
OS4mainHUB <os4.official@gmail.com>
2022-03-16 20:05:54 +0700

    Add Splash

--- app/src/main/java/com/example/alefimage/views/MainActivity.kt
+++ app/src/main/java/com/example/alefimage/views/MainActivity.kt
@@ -1,10 +1,12 @@
-package com.example.alefimage
+package com.example.alefimage.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.alefimage.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_AlefImage)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
